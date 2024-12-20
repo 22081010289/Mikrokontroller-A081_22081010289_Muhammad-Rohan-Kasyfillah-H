905df771e2b9d57f80e667c3d141487873accc79
Muhammad Rohan Kasyfillah H._22081010289 <161286844+22081010289@users.noreply.github.com>
2024-12-20 15:34:50 +0700

    Update PWM_Testing_Program.ino

--- PWM_Testing_Program.ino
+++ PWM_Testing_Program.ino
@@ -1,23 +1,32 @@
+// the number of the LED pin
 const int ledPin = 26;
 
+// setting PWM properties
 const int freq = 5000;
 const int ledChannel = 0;
-const int resolution = 8:
-
+const int resolution = 8;
+ 
 void setup(){
+  // configure LED PWM functionalitites
   ledcSetup(ledChannel, freq, resolution);
-
-  ledAttachPin(ledPin, ledChannel);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
-
+ 
 void loop(){
-  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
     ledcWrite(ledChannel, dutyCycle);
     delay(20);
   }
 
+  // decrease the LED brightness
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    ledcWrite(ledChannel, dutyCycle);
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
     delay(20);
   }
 }
+
